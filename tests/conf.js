1016e07e699511247d761fb6a75af97cadc7968e
williamfreitas217 <williamdov.c.f@gmail.com>
2019-08-17 12:25:14 -0400

    More tests

--- tests/conf.js
+++ tests/conf.js
@@ -3,7 +3,7 @@ var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['test_date_spec.js'],
+    specs: ['test_date_spec.js','test_alert_spec.js', 'test_random_spec.js'],
     capabilities: {
         'browserName': 'firefox'
     },
